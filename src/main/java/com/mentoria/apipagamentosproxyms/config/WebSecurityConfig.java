c905828da1b1e8eb179ec03a1b51dfdbebdea9f9
Joao Santana <joao.santana@sensedia.com>
2023-06-16 08:55:36 -0300

    Alterações nas configurações

--- src/main/java/com/mentoria/apipagamentosproxyms/config/WebSecurityConfig.java
+++ src/main/java/com/mentoria/apipagamentosproxyms/config/WebSecurityConfig.java
@@ -2,27 +2,26 @@ package com.mentoria.apipagamentosproxyms.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-    
-    public static final String USER = "user";
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .requestMatchers(HttpMethod.POST, "/pagamentos").hasAnyRole(USER)
-                .anyRequest().authenticated();
+        
+        http
+            .csrf().disable() // 
+            .authorizeHttpRequests().anyRequest().authenticated();
+
+        http.oauth2ResourceServer().jwt();
+
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        
         return http.build();
     }
 
